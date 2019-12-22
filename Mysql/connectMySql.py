5e1b45a206e181ad79c51da39646de67745cdb8a
amirganmor <amirganmor@gmail.com>
2019-12-22 11:41:43 +0200

    Update connectMySql.py

--- Mysql/connectMySql.py
+++ Mysql/connectMySql.py
@@ -1,17 +1,18 @@
 import mysql.connector
 
-table1='lmntr_2_edd_activity_log_temp'
+
 mydb = mysql.connector.connect (
-    host = "localhost",
-    user ="root",
-    passwd="theicea13",
+    database='website_db' , 
+    host = "35.246.223.158",
+    user ="data_team",
+    passwd="eV5*mQV(tB",
     port="3306",
 )
 #print(mydb)
 
 curs = mydb.cursor()
 #query = "show tables" 
-query = "SELECT * FROM sakila.actor"
+query = "SELECT * FROM website_db.affiliate_wp_affiliatemeta"
 curs.execute(query)
 results = curs.fetchall()
 print(results)
