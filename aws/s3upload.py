7b50b0dcfeb1f2f3dd3ab9ff8a0d667ad45c2b1a
amirganmor <amirganmor@gmail.com>
2019-12-24 12:07:52 +0200

    Update s3upload.py

--- aws/s3upload.py
+++ aws/s3upload.py
@@ -1,8 +1,8 @@
 import boto3
 from botocore.client import Config
 
-ACCESS_KEY_ID = 'AKIAQ4U7V4757RIV375Q'
-ACCESS_SECRET_KEY = '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'
+ACCESS_KEY_ID = 'xxxxxxxxxxxxxxxxxxxxxxx'
+ACCESS_SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxx'
 BUCKET_NAME = 'elementor-json-tables'
 FILE_NAME = 'aaa.txt'
 
