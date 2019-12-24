f2ec273461be15a873b22cf46588aea79200ea79
amirganmor <amirganmor@gmail.com>
2019-12-24 12:01:15 +0200

    Create s3upload.py

---
+++ aws/s3upload.py
@@ -0,0 +1,22 @@
+import boto3
+from botocore.client import Config
+
+ACCESS_KEY_ID = 'AKIAQ4U7V4757RIV375Q'
+ACCESS_SECRET_KEY = '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'
+BUCKET_NAME = 'elementor-json-tables'
+FILE_NAME = 'aaa.txt'
+
+
+data = open(FILE_NAME, 'rb')
+
+# S3 Connect
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+)
+
+# Image Uploaded
+s3.Bucket(BUCKET_NAME).put_object(Key=FILE_NAME, Body=data, ACL='public-read')
+print ("Done")
