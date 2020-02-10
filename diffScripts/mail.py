f87b8cd180eb2838e6b6b4a476cd3fa264e12fc9
amirganmor <amirganmor@gmail.com>
2020-02-10 17:53:54 +0200

    Create mail.py

---
+++ diffScripts/mail.py
@@ -0,0 +1,28 @@
+import smtplib
+    
+def send_email(user, password, recipient, subject, body):
+
+    gmail_user = user
+    gmail_pwd = password
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_user, gmail_pwd)
+    server.sendmail(FROM, TO, message)
+    server.close()
+
+user="amirganmor@gmail.com"
+pwd="*********"
+recipient="amir@elementor.com"
+subject="try"
+body="try amir fdsfsdfsdf"
+send_email(user, pwd, recipient, subject, body)
